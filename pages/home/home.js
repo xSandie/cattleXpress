06de3b15730d230da801c35e49c35eed756b42e8
向书晗 <sandiexiang@foxmail.com>
2018-05-23 09:47:15 +0800

    Initial commit

---
+++ pages/home/home.js
@@ -0,0 +1,82 @@
+Page({
+
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        mySchoolName: '陕西师范大学（长安校区）',
+        reward: '99',
+        loctionSrc: "../../images/location.png",
+        pubIcon: '../../images/publisher.png',
+        exInstance: '申通快递·阳光苑',
+        sdInstance: '宿舍区 硕士楼',
+        exWorry: true,
+        exWeight: '<1KG',
+        exSize: '小件',
+        exExTime: '05-07 18:00',
+        pubName: '向同学 41612057',
+
+        listCount: [
+            '1',
+            '2',
+            '3',
+            '4',
+            '5',
+            '6'
+        ]
+    },
+
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad: function(options) {},
+
+    /**
+     * 生命周期函数--监听页面初次渲染完成
+     */
+    onReady: function() {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面显示
+     */
+    onShow: function() {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面隐藏
+     */
+    onHide: function() {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面卸载
+     */
+    onUnload: function() {
+
+    },
+
+    /**
+     * 页面相关事件处理函数--监听用户下拉动作
+     */
+    onPullDownRefresh: function() {
+
+    },
+
+    /**
+     * 页面上拉触底事件的处理函数
+     */
+    onReachBottom: function() {
+
+    },
+
+    /**
+     * 用户点击右上角分享
+     */
+    onShareAppMessage: function() {
+
+    }
+})
