e3c264bf914a8b8d458beed2bc2c2fb3c699e7ab
xSandie <sandiexiang@foxmail.com>
2019-07-27 16:44:58 +0800

    新增吐个槽社区

--- pages/my/my.js
+++ pages/my/my.js
@@ -7,6 +7,16 @@ Page({
      * 页面的初始数据
      */
     data: {
+        appId: "wx8abaf00ee8c3202e",
+        extraData: {
+            // 把1221数字换成你的产品ID，否则会跳到别的产品
+            id : "72261",
+            // 自定义参数，具体参考文档
+            customData : {
+                clientInfo: `iPhone OS 10.3.1 / 3.2.0.43 / 0`,
+                imei: '7280BECE2FC29544172A2B858E9E90D0'
+            }
+        },
         cardBg: "../../images/bigback.png",
         balance: null,
         creditScore: 0,
